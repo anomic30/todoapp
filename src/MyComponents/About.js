379b800dc9bbbfde209c8f50f038a16daec558ef
[Anom Chakravorty] <anomchakravorty3008@gmail.com>
2021-05-02 16:39:52 +0530

    changes

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import './About.css';
 import profileImage from '../img/pic2.svg';
-// import '../../public/pic2.svg'
+// import '../../public/pic3.svg'
 
 export const About = () => {
     return (
-        <div class="whole">
-        <div class="container1">
-            <div class="left-side">
-                <img src={profileImage} alt=""/>
-            </div>
-            <div class="right-side">
-                <h2>Developer</h2>
-                <div class="name">
-                    <h1>Anom Chakravorty</h1>
-                </div>
-                <span></span>
-                <p>Hey everyone, this is my first React application. I hope you liked it as much as I do. <br></br>
-            Do follow me in the below mentioned social media and platforms...</p>
+        <div class="main">
+        <div class="left-main">
+            <img src={profileImage} alt="avatar"/>
+        </div>
+        <div class="right-main">
+            <h1>Hey there, its me Anom</h1>
+            <p>This is my first React app,
+                I hope you would like this as much as I do!
+                <br></br>
+                Do follow me in the below mentioned social medias...
+            </p>
             <div class="icons">
                 <a href="https://github.com/anomic30"><i class="fa fa-github" ></i></a>
                 <a href="https://www.linkedin.com/in/anomic/"><i class="fa fa-linkedin" ></i></a>
                 <a href="https://www.instagram.com/codermav/"><i class="fa fa-instagram" ></i></a>
             </div>
-            </div>
-            </div>
+        </div>
         </div>
     )
 }
